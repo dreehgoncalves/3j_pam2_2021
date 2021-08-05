5e824426971e24f403747d667423de2caba98bd4
dreehgoncalves <andressagoncalves1997@gmail.com>
2021-08-04 22:52:38 -0300

    atuallização do index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const servidor = express()
 
+servidor.get(
+    '/', (req, res, next) => {
+        return res.send({
+            "mensagem": "Tá logado ein bichão"
+        })
+    }
+)
+
 servidor.listen(3000, () => {
-    console.log('Servidor funcionando!')
+    console.log('Servidor na halfpipe!')
 })
