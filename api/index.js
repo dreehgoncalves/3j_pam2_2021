6adcbb2097bbd1186cdcae77d8a2f1b97c4b8642
dreehgoncalves <andressagoncalves1997@gmail.com>
2021-09-01 20:21:09 -0300

    alteração de index.js

--- api/index.js
+++ api/index.js
@@ -1,27 +1,57 @@
-const express = require('express')
-const servidor = express()
-const mysql = require('mysql2')
+const express = require("express");
+const servidor = express();
+const mysql = require("mysql2");
 const banco = mysql.createPool({
-    "database": "3j_2021",
-    "user": "root",
-    "password": "",
-    "host": "localhost",
-    "port": "3306"
+    database: "3j_2021",
+    user: "root",
+    password: "",
+    host: "localhost",
+    port: "3306",
+});
+
+servidor.delete("/cliente/:id", (req, res, next) => {
+    let id = req.params.id
+
+    return res.status(200).send({
+        Verbo: 'delete',
+        Mensagem: `Id capturado ${id}`
+    })
 })
-servidor.get("/teste", (req, res, next) => {
+
+servidor.patch("/cliente/:id", (req, res, next) => {
+    let id = req.params.id
+
+    return res.status(200).send({
+        Verbo: 'patch',
+        Mensagem: `Id capturado ${id}`
+    })
+})
+
+servidor.get("/cliente/:id", (req, res, next) => {
+    let id = req.params.id
+
+    return res.status(200).send({
+        Verbo: 'get',
+        Mensagem: `Id capturado ${id}`
+    })
+})
+
+servidor.get("/testarconexao", (req, res, next) => {
     banco.getConnection((error, conn) => {
         if (error) {
             return res.status(500).send({
                 Erro: "Erro no servidor",
                 Detalhes: error,
-            })
+            });
         }
-        conn.release()
+
+        conn.release();
+
         return res.status(200).send({
             Mensagem: "Conexão estabelecida com sucesso",
-        })
-    })
-})
+        });
+    });
+});
 
 servidor.get("/clientes", (req, res, next) => {
     const QUERY = 'SELECT * FROM clientes ORDER BY nome'
@@ -29,19 +59,21 @@ servidor.get("/clientes", (req, res, next) => {
     banco.getConnection((error, conn) => {
         if (error) {
             return res.status(500).send({
-                Erro: "Não foi possivel atender à solicitação",
+                Erro: "Não foi possível atender à solicitação",
                 Detalhes: error
             })
         }
+
         conn.query(QUERY, (error, resultado) => {
             conn.release()
 
             if (error) {
                 return res.status(500).send({
-                    Erro: "Não foi possivel atender à solicitação",
+                    Erro: "Não foi possível atender à solicitação",
                     Detalhes: error
                 })
             }
+
             return res.status(200).send({
                 Mensagem: "Dados retornados com sucesso",
                 Dados: resultado
@@ -52,40 +84,42 @@ servidor.get("/clientes", (req, res, next) => {
 
 servidor.get("/clientes/:criterio", (req, res, next) => {
     let criterio = req.params.criterio
-    const QUERY = `SELECT * FROM clientes WHERE nome like '%${criterio}%' ORDER BY nome`
+    const QUERY = `SELECT * FROM clientes WHERE nome LIKE '%${criterio}%' ORDER BY nome`
 
     banco.getConnection((error, conn) => {
         if (error) {
             return res.status(500).send({
-                Erro: "Não foi possivel atender à solicitação",
+                Erro: "Não foi possível atender à solicitação",
                 Detalhes: error
             })
         }
+
         conn.query(QUERY, (error, resultado) => {
             conn.release()
 
             if (error) {
                 return res.status(500).send({
-                    Erro: "Não foi possivel atender à solicitação",
+                    Erro: "Não foi possível atender à solicitação",
                     Detalhes: error
                 })
             }
+
             return res.status(200).send({
-                Mensagem: "Dados retornados com sucesso",
+                Mensagem: "Consulta realizada com sucesso",
                 Dados: resultado
             })
         })
     })
 })
 
-servidor.get(
-    '/', (req, res, next) => {
-        return res.send({
-            "mensagem": "Tá logado ein bichão"
-        })
-    }
-)
+servidor.get("/", (req, res, next) => {
+    return res.send({
+        mensagem: "Bem-vindo(a) ao servidor",
+        cidade: "Itapeva",
+        uf: "SP",
+    });
+});
 
 servidor.listen(3000, () => {
-    console.log('Servidor na halfpipe!')
-})
+    console.log("Servidor funcionando!");
+});
